03954ea687e1aad4620fe9d62931e1d5e6a0be3f
Dominic Zijlstra <d.zijlstra@iwoca.co.uk>
2019-01-24 10:16:04 +0000

    Add comments. Add some convenience functions. Allow any function for scorer

--- fasttrees/fasttrees.py
+++ fasttrees/fasttrees.py
@@ -5,21 +5,20 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics.scorer import balanced_accuracy_score
 import operator
 
-
 construction_algorithms = ['marginal_fan']
-maximize_metrics = ['bacc']
 operator_dict = {'<=': operator.le, '>': operator.gt, '==': operator.eq, 'in': lambda val, lst: val in lst}
 
 
 class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
-    """FFT classifier"""
+    """Fast-and-Frugal-Tree classifier"""
 
-    def __init__(self, construction_algorithm='marginal_fan', maximize_metric='bacc', max_levels=4, stopping_param=.1):
-        """Inits FFT classifier.
+    def __init__(self, construction_algorithm='marginal_fan', scorer=balanced_accuracy_score, max_levels=4,
+                 stopping_param=.1):
+        """Inits Fast-and-Frugal-Tree classifier.
 
         Args:
             construction_algorithm: algorithm used to create trees. Currently supported: 'marginal_fan'
-            maximize_metric: metric to maximize when choosing threshold. Currently supported: 'bacc'
+            scorer: metric to maximize when choosing threshold. Any function that returns higher values for better predictions
             max_levels: maximum number of levels for possible trees
             stopping_param: prune levels containing less than stopping_param of cases
 
@@ -32,41 +31,55 @@ class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
             raise ValueError(
                 'Not a valid construction_algorithm. Possible choices are {}'.format(construction_algorithms))
 
-        if maximize_metric in maximize_metric:
-            self.maximize_metric = maximize_metric
-        else:
-            raise ValueError('Not a valid maximize_metric. Possible choices are {}'.format(maximize_metric))
+        self.scorer = scorer
 
         self.max_levels = int(max_levels)
 
         self.stopping_param = float(stopping_param)
 
     def _score(self, y, predictions):
-        if self.maximize_metric == 'bacc':
-            return balanced_accuracy_score(y, predictions)
-
-    def _get_thresholds(self, X, y):
-        """Get thresholds and directions that maximimize maximize_metric for each feature.
+        """Scores predictions against y.
 
         Args:
-            X: Dataframe with features as columns
-            y: true values
+            y: real outcomes
+            predictions: predicted outcomes
 
         Returns:
-            Dataframe with row for every feature, with threshold, direction and maximize_metric, sorted by maximize_metric
+            Score
         """
-        threshold_df = pd.DataFrame(columns=['feature', 'type', 'threshold', 'direction', self.maximize_metric])
+        return self.scorer(y, predictions)
+
+    def _get_thresholds(self, X, y):
+        """Get thresholds and directions that maximimize scorer for each feature.
+
+        Args:
+            X: Dataframe with features as columns. Features can be numerical or categorical
+            y: real, binary, outcomes.
+
+        Returns:
+            self.thresholds: Dataframe with row for every feature, with threshold, direction
+            and scorer, sorted by scorer
+        """
+        threshold_df = pd.DataFrame(columns=['feature', 'type', 'threshold', 'direction', self.scorer.__name__])
+
+        # Get optimal classification threshold for each feature
         for i, col in enumerate(X):
             threshold_df.loc[i, 'feature'] = col
+            metric_max = 0
+
             if X[col].dtype.name == 'category':
                 # categorical
                 threshold_df.loc[i, 'type'] = 'categorical'
                 categories = X[col].cat.categories
-                metric_max = 0
+
+                # try all possible subsets of categories
                 for l in range(1, len(categories)):
                     for subset in itertools.combinations(categories, l):
                         predictions = X[col].isin(subset)
                         metric = self._score(y, predictions)
+
+                        # save metric, direction and threshold if they are the best so far
+                        # TODO save all metrics instead
                         if metric >= metric_max:
                             metric_max = metric
                             direction_max = 'in'
@@ -75,22 +88,30 @@ class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
                 # numerical
                 threshold_df.loc[i, 'type'] = 'numerical'
                 test_values = X[col].unique()
-                metric_max = 0
+
+                # try smaller than and bigger than for every unique value in column
                 for val in test_values:
                     for direction, operator in {op: operator_dict[op] for op in ['<=', '>']}.items():
                         predictions = operator(X[col], val)
                         metric = self._score(y, predictions)
+
+                        # save metric, direction and threshold if they are the best so far
                         if metric >= metric_max:
                             metric_max = metric
                             direction_max = direction
                             threshold = val
+
             threshold_df.loc[i, 'threshold'] = threshold
             threshold_df.loc[i, 'direction'] = direction_max
-            threshold_df.loc[i, self.maximize_metric] = metric_max
-        threshold_df[self.maximize_metric] = threshold_df[self.maximize_metric].astype(float)
-        self.thresholds = threshold_df.sort_values(by=self.maximize_metric, ascending=False).reset_index(drop=True)
+            threshold_df.loc[i, self.scorer.__name__] = metric_max
 
-    def _predict_all(self, X, cue_df):
+        threshold_df[self.scorer.__name__] = threshold_df[self.scorer.__name__].astype(float)
+
+        # sort features by their score
+        self.thresholds = threshold_df.sort_values(by=self.scorer.__name__, ascending=False).reset_index(drop=True)
+
+    @staticmethod
+    def _predict_all(X, cue_df):
         """Make predictions for X given cue_df.
 
         Args:
@@ -98,28 +119,58 @@ class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
             cue_df: Dataframe with ordered features, directions, thresholds, exits
 
         Returns:
-            Dataframe with row for every feature, with threshold, direction and maximize_metric, sorted by maximize_metric
+            Series with prediction for every cue in cue_df up to the point the fast-and-frugal-tree was exited
         """
         nr_rows = cue_df.shape[0]
 
         def prediction_func(row):
+            """Look up the row's features in order of their score. Exit if the threshold is met and the tree exits on True,
+            or if the threshold is not met and the tree exits on False.
+
+            Args:
+                row: Dataframe row with features as columns
+
+            Returns:
+                Series with prediction for all cues used
+            """
             for index, cue_row in cue_df.iterrows():
                 operator = operator_dict[cue_row['direction']]
                 outcome = operator(row[cue_row['feature']], cue_row['threshold'])
+
+                # store prediction in series
                 row.set_value(index, outcome)
-                if ((cue_row['exit'] == 1) and outcome) or ((cue_row['exit'] == 0) and not outcome) or (
-                        index + 1 == nr_rows):
+
+                # exit tree if outcome is exit or last cue reached
+                if (cue_row['exit'] == int(outcome)) or (index + 1 == nr_rows):
                     cues_used = index + 1
                     break
 
+            # return predictions for cues used
             return row[-cues_used:]
 
         all_predictions = X.apply(prediction_func, axis=1)
         return all_predictions
 
-    def _predict_and_prune(self, X, cue_df):
+    @staticmethod
+    def _get_final_prediction(all_predictions):
+        """Get final (latest non-null) predictions from all cue predictions.
+
+        Args:
+            X: Dataframe with all predictions
+
+        Returns:
+            Dataframe with final prediction
         """
-            Make predictions and prune features that classify less than stopping_param
+        return all_predictions.ffill(axis=1).iloc[:, -1]
+
+    def _predict_and_prune(self, X, cue_df):
+        """Make predictions and prune features that classify less than stopping_param.
+
+        Args:
+            X: Dataframe with all predictions
+
+        Returns:
+            Dataframe with pruned prediction
         """
         all_predictions = self._predict_all(X, cue_df)
 
@@ -129,25 +180,31 @@ class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
         all_predictions = all_predictions[cols]
 
         # get last prediction
-        predictions = all_predictions.ffill(axis=1).iloc[:, -1]
+        predictions = self._get_final_prediction(all_predictions)
 
         nr_cues_used = len(cols)
 
         return predictions, nr_cues_used
 
     def _growtrees(self, X, y):
-        """
-        Grow all possible trees up to self.max_levels. Prune levels classifying less than self.stopping_param
+        """Grow all possible trees up to self.max_levels. Prune levels classifying less than self.stopping_param
+
+        Args:
+            X: Dataframe with features as columns. Features can be numerical or categorical
+            y: real, binary, outcomes.
+
+        Returns:
+            self.all_trees: Dataframe with all trees grown
         """
         relevant_features = self.thresholds.head(self.max_levels)
         midx = pd.MultiIndex(levels=[[], []],
                              labels=[[], []],
                              names=['tree', 'idx'])
-        tree_df = pd.DataFrame(columns=['feature', 'type', 'threshold', 'direction', self.maximize_metric], index=midx)
+        tree_df = pd.DataFrame(columns=['feature', 'type', 'threshold', 'direction', self.scorer.__name__], index=midx)
         for tree in range(2 ** (self.max_levels - 1)):
             for index, feature_row in relevant_features.iterrows():
                 tree_df.loc[
-                    (tree, index), ['feature', 'type', 'threshold', 'direction', self.maximize_metric]] = feature_row
+                    (tree, index), ['feature', 'type', 'threshold', 'direction', self.scorer.__name__]] = feature_row
 
                 # exit 0.5 is stop, exit 1 means stop on true, exit 0 means stop on false
                 tree_df.loc[(tree, index), 'exit'] = np.binary_repr(tree, width=self.max_levels)[-1 - index]
@@ -160,24 +217,60 @@ class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
                 tree_df.drop(index=(tree, i), inplace=True)
             tree_df.loc[(tree, nr_cues_used - 1), 'exit'] = 0.5
 
-            tree_df.loc[tree, self.maximize_metric] = self._score(y, predictions)
+            tree_df.loc[tree, self.scorer.__name__] = self._score(y, predictions)
 
-        self._all_trees = tree_df
+        self.all_trees = tree_df
+
+    def get_tree(self, idx=None):
+        """Get specific tree from all trees
+
+        Args:
+            idx: index of desired tree. Will return best tree if None
+
+        Returns:
+            Dataframe of tree
+        """
+        if idx is None:
+            return self.all_trees.loc[self.all_trees[self.scorer.__name__].idxmax()[0]]
+        else:
+            return self.all_trees.loc[idx]
 
     def fit(self, X, y):
-        """
-        Fit FFT
+        """Fits the classifier to the data.
+
+        Args:
+            X: Dataframe with features as columns. Features can be numerical or categorical
+            y: real, binary, outcomes.
+
+        Returns:
+            self: Fitted FastFrugalTreeClassifier
         """
         self._get_thresholds(X, y)
         self._growtrees(X, y)
-        self._best_tree = self._all_trees[
-            self._all_trees[self.maximize_metric] == self._all_trees[self.maximize_metric].max()]
-        self._best_tree.index = self._best_tree.index.droplevel(level=0)
+        self.best_tree = self.get_tree()
         return self
 
-    def predict(self, X):
-        all_predictions = self._predict_all(X, self._best_tree)
-        return all_predictions.ffill(axis=1).iloc[:, -1]
+    def predict(self, X, tree_idx=None):
+        """Predicts outcomes for data X.
+
+        Args:
+            X: Dataframe with features as columns. Features can be numerical or categorical
+            tree_idx: tree to use, default is best tree
+
+        Returns:
+            predictions
+        """
+        all_predictions = self._predict_all(X, self.get_tree(tree_idx))
+        return self._get_final_prediction(all_predictions)
 
     def score(self, X, y=None):
+        """Predicts for data X. Scores predictions against y.
+
+        Args:
+            X: Dataframe with features as columns. Features can be numerical or categorical
+            y: real outcomes
+
+        Returns:
+            Score
+        """
         return self._score(y, self.predict(X))
