75a2a383787d16e4232cc00b494f5d45b3c8e345
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-11-21 16:35:11 +0100

    Moved long description grabing code into its own function.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 
-
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def get_long_description():
+    # read the contents of your README file
+    from os import path
+    this_directory = path.abspath(path.dirname(__file__))
+        with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+            long_description = f.read()
+    return long_description
 
 
 def get_version():
