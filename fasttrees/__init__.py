5c1e7ef2769f3377d31ddc2500f27db7ea3e68b9
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2023-11-19 19:48:06 +0100

    feat(version): bump version

--- fasttrees/__init__.py
+++ fasttrees/__init__.py
@@ -2,5 +2,5 @@ __description__ = "A fast and frugal tree classifier for sklearn"
 __author__ = "Dominic Zijlstra, Stefan Bachhofner"
 
 __license__ = "MIT"
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 __author_email__ = "dominiczijlstra@gmail.com, bachhofner.dev@gmail.com"
