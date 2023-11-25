e06016cf02acdeed0afa222bc26aadb1e0110d2a
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2023-11-25 23:04:26 +0100

    feat(fasttrees): add instance variables that store the training data set that is seen when fit is called

--- fasttrees/fasttrees.py
+++ fasttrees/fasttrees.py
@@ -10,6 +10,7 @@ from typing import Callable, Tuple
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics.scorer import _BaseScorer, balanced_accuracy_score
 
 
@@ -427,6 +428,11 @@ class FastFrugalTreeClassifier(BaseEstimator, ClassifierMixin):
         self._get_best_thresholds()
         self._growtrees(X, y)
         self.best_tree = self.get_tree()
+
+        # store the training data set seen during training
+        self.classes_ = unique_labels(y)
+        self.X_ = X
+        self.y_ = y
         return self
 
     def _validate_hyperparameters(self) -> None:
