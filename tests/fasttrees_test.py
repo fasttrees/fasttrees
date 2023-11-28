0ecf4560d7d39836afa94eeb7829d2a5b7bba292
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2023-11-28 13:49:19 +0100

    tests(classifier): add test for created tree

--- tests/fasttrees_test.py
+++ tests/fasttrees_test.py
@@ -6,6 +6,10 @@ from sklearn import datasets, model_selection
 from sklearn.utils.estimator_checks import check_estimator
 import pytest
 
+import pathlib
+
+import pandas as pd
+
 from fasttrees import FastFrugalTreeClassifier
 
 
@@ -45,3 +49,48 @@ def test_classification(X, y):
 
     assert fftc.score(X_iris_train, y_iris_train) > 0.6
     assert fftc.score(X_iris_test, y_iris_test) > 0.6
+
+
+def test_classification_heart_disease():
+    '''Test fast-and-frugal classifier on the heart disease data set.
+
+    The purpose is to test whether we get the same results as with the R implementation
+    '''
+    df_expected = pd.DataFrame(
+        data={'IF NO': ['↓', 'decide NO', 'decide NO'],
+              'feature': ['thal', 'cp', 'ca'],
+              'direction': ['in', 'in', 'in'],
+              'threshold': ["('fd', 'rd')", "('a',)", "('1', '2', '3')"],
+              'IF YES': ['decide YES', '↓', 'decide YES']
+    })
+    df_expected.index.name = 'idx'
+
+    df_train = pd.read_csv(
+        pathlib.Path('./data/heartdisease_train.csv'),
+        dtype={
+            'diagnosis': 'bool', # target
+            'age': 'int',
+            'sex': 'bool',
+            'cp': 'category',
+            'trestbps': 'int',
+            'chol': 'int',
+            'fbs': 'bool',
+            'restecg': 'category',
+            'thalach': 'int',
+            'exang': 'bool',
+            'oldpeak': 'float',
+            'slope': 'category',
+            'ca': 'category',
+            'thal': 'category'
+    })
+
+    X, y = df_train.drop('diagnosis', axis=1), df_train['diagnosis']
+
+    fftc = FastFrugalTreeClassifier()
+    fftc.fit(X, y)
+
+    y_pred = fftc.predict(X)
+    assert y_pred.shape == (X.shape[0],)
+
+    pd.testing.assert_frame_equal(fftc.get_tree().astype({'threshold': str}),
+                                  df_expected)
