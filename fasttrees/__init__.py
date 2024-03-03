ab92c8a49d28957913ec9290c0fa074ca053714a
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2024-03-03 16:08:15 +0100

    fix(build): move to pbr for version string and remove other metadata

--- fasttrees/__init__.py
+++ fasttrees/__init__.py
@@ -1,17 +1,9 @@
 '''
 The :mod:`fasttrees` module includes the fast-and-frugal tree classifier.
 '''
-from importlib.metadata import distribution
+from pbr.version import VersionInfo
 
-
-fasttrees_metadata = dict(distribution('fasttrees').metadata)
-
-__version__ = fasttrees_metadata['Version']
-__description__ = fasttrees_metadata['Summary']
-__license__ = fasttrees_metadata['License']
-
-__author__ = fasttrees_metadata['Author']
-__author_email__ = fasttrees_metadata['Author-email']
+__version__ = VersionInfo('fasttrees').release_string()
 
 
 
