681bf438f1b9f02cc84e73c44bb27badcf19d08a
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2024-05-16 18:15:43 +0200

    doc(tests): add licence and copyright notice

--- tests/fasttrees_test.py
+++ tests/fasttrees_test.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Dominic Zijlstra <dominiczijlstra@gmail.com>, Stefan Bachhofner <bachhofner.dev@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
 '''
 Testing for Fast-And-Frugal tree module (fasttrees)
 '''
